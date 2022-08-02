73fa3cf31b9ec3b7a7a9602f34f3d4fa0de2b372
Juniorzeus <dkssnd3318@naver.com>
2022-08-03 00:44:06 +0900

    one code

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
-print("hello")
-print("two hello")
-print("7777777777")
-print("7777777777")
-print("7777777777")
-print("7777777777")
-print("7777777777")
+from re import A
+
+
+a =1 
+b =2
+c =0 
+print(a,b)
+c = a 
+a = b 
+b = c
+print( a , b ) # 2 1 
---
+++ test.java
@@ -0,0 +1,7 @@
+public class test   {
+    public static void public static void main(String[] args) {
+        int aa[][] = { {46 , 47 ,49} , {72 }};
+        System.out.print(aa[0].length);
+
+    }
+}
